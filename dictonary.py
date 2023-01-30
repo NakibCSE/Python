a9eb79e3c1fd336ed622b598a493ee6e035d1793
nakibcse <nakkibcse@gmail.com>
2023-01-30 16:58:49 +0600

    dictionary practice

---
+++ dictonary.py
@@ -0,0 +1,7 @@
+marks = [777,76,65,78,62,64,60,77,75,79]
+roll = input("Please enter your roll number: ")
+print("Your marks is ",marks[int(roll)-1])
+
+
+marks2 =[[74,73],[70,75],[68,72],[73,73]]
+marks2[0]
